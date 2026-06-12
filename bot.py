c06306104cec777ba2567e14effc7c14ef1b65cc
krop364 <kropanevaoo@gmail.com>
2026-06-12 19:53:59 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+from pyrogram.types import ReplyKeyboardMarkup
+
+# ВСТАВЬТЕ ВАШИ ДАННЫЕ ВМЕСТО ЗНАЧЕНИЙ НИЖЕ:
+api_id = 35051665  # Ваш api_id с my.telegram.org (число)
+api_hash = "32f3b364d6587b554b108a0e8fb9c6db"  # Ваш api_hash с my.telegram.org (в кавычках)
+bot_token = os.environ.get("TELEGRAM_TOKEN")  # Ваш токен от @BotFather
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    keyboard = ReplyKeyboardMarkup([
+        ["🔘 Кто Гусь?", "🔘 Кто Олька?"],
+        ["🔘 Как играем?"]
+    ], resize_keyboard=True)
+    
+    await message.reply("Выберите:", reply_markup=keyboard)
+
+@app.on_message(filters.text)
+async def buttons(client, message):
+    if message.text == "🔘 Кто Гусь?":
+        await message.reply("лох вонючий")
+    elif message.text == "🔘 Кто Олька?":
+        await message.reply("Цариииииица")
+    elif message.text == "🔘 Как играем?":
+        await message.reply("Мы ахуенно играем, невероятно сильно")
+
+print("✅ Бот запущен!")
+app.run()
